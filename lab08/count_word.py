c77b28bea7a6822698a55bfffff2e883276c2c9b
Jimmy Liu_Vlab <jimmy.liu@mail.com>
2022-07-23 18:07:48 +1000

    优化了一下lab08/count_word.py的代码

--- lab08/count_word.py
+++ lab08/count_word.py
@@ -5,5 +5,5 @@ import sys, re
 pattern = sys.argv[1]
 contents = sys.stdin.read()
 
-match_list = re.findall(r'\b' + pattern + r'\b', contents, flags = re.I)
-print(f"{pattern} occurred {len(match_list)} times")
+match_count = len(re.findall(r'\b' + pattern + r'\b', contents, flags = re.I))
+print(f"{pattern} occurred {match_count} times")
